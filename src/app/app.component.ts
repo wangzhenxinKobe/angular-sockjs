baf2ceff51aa936da2f35478a91594db28ecf8c8
Gavin Yu <yuweihua@reganfund.com>
2017-06-04 08:50:20 +0800

    websocket for greeting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+
+declare let SockJS;
+declare let Stomp;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  host : string = "http://localhost:8080";
+  bConnected : boolean = false;
+  stompClient : any = null;
+
+  name : string = "";
+
+  messages : string[] = [];
+
+  constructor(){}
+
+  onClickConnBtn() {
+
+    if(this.bConnected) { //断开链接
+
+      if (this.bConnected && this.stompClient != null) {
+        this.stompClient.disconnect();
+      }
+      this.bConnected = false;
+      console.log("Disconnected");
+
+    } else {
+
+      var socket = new SockJS(this.host + '/gs-guide-websocket');
+      this.stompClient = Stomp.over(socket);
+      this.stompClient.connect({}, (frame) => {
+        this.bConnected = true;
+        console.log('Connected: ' + frame);
+        this.stompClient.subscribe('/topic/greetings',  greeting => {
+          let content = JSON.parse(greeting.body).content;
+          console.info('Receiving : ' + content);
+          this.messages.push(content);
+        });
+      });
+
+    }
+
+  }
+
+  send() {
+
+    this.stompClient.send("/app/hello", {}, JSON.stringify({'name': this.name}));
+
+  }
+
 }
